e717df80bc2dce2a3b1dbf317f856afad7c9393b
SujiPrasanth <sujiprasantharumugam@gmail.com>
2026-05-29 13:52:11 +0530

    Fix trust proxy configuration

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
 
-app.set("proxy trusted", 1)
+app.set("trust proxy", 1)
 
 app.use(session({
     secret: process.env.SESSION_SECRET,
